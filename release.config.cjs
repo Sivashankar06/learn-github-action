460876a25de5b9d4e49aefc224e82e1cca8503e3
Sivashankar <siva.a.shankar@gmail.com>
2024-09-28 15:29:55 +0530

    fix: Update release cofig

--- release.config.cjs
+++ release.config.cjs
@@ -1,3 +1,21 @@
-module.exports = {
-    branches: ["main", { name: "next", prerelease: true }]
-}
+const config = {
+    branches: ["main", { name: "next", prerelease: true }],
+    preset: "angular",
+    tagFormat: "v${version}",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/github", {
+                failTitle: false,
+                labels: false,
+                releasedLabels: false,
+                successComment:
+                    ":tada: A prerelease with version ${nextRelease.version} has been published.\n"
+            },
+        ],
+        "semantic-release-yarn",
+    ],
+};
+
+module.exports = config;
