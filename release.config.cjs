1c0d09dad091f97ec48283043b6aaa95550a70cd
Sivashankar <siva.a.shankar@gmail.com>
2024-09-29 23:23:14 +0530

    Config file update

--- release.config.cjs
+++ release.config.cjs
@@ -1,18 +1,17 @@
 const config = {
     branches: ["main", { name: "next", prerelease: true }],
     plugins: [
-        "@semantic-release/commit-analyzer",
-        {
-            "preset": "angular",
-            "releaseRules": [
-              { "type": "docs", "scope": "README", "release": "patch" },
-              { "type": "refactor", "release": "minor" },
-              { "type": "revert", "release": "major", "tag": "revert" }
-            ],
-            "parserOpts": {
-              "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
-            }
-        },
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                "preset": "angular",
+                "releaseRules": [
+                { "type": "docs", "scope": "README", "release": "patch" },
+                { "type": "refactor", "release": "minor" },
+                { "type": "revert", "release": "major", "tag": "revert" }
+                ]
+            },
+        ],  
         "@semantic-release/release-notes-generator"
     ],
 };
