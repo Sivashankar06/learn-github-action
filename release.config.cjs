6b96a46f4f02a735bad4e6047da93d624b6e0631
Sivashankar <siva.a.shankar@gmail.com>
2024-09-29 22:46:34 +0530

    revert: for a major release

--- release.config.cjs
+++ release.config.cjs
@@ -3,10 +3,11 @@ const config = {
     plugins: [
         "@semantic-release/commit-analyzer",
         {
+            "preset": "angular",
             "releaseRules": [
               { "type": "docs", "scope": "README", "release": "patch" },
               { "type": "refactor", "release": "minor" },
-              { "type": "revert", "release": "patch" }
+              { "type": "revert", "release": "major", "tag": "revert" }
             ],
             "parserOpts": {
               "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
