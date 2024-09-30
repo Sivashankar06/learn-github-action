9b378ced8d61c0337c04fb3660450c316dd58ac2
Sivashankar <siva.a.shankar@gmail.com>
2024-10-01 01:38:58 +0530

    fix: Adding PR comment

--- release.config.cjs
+++ release.config.cjs
@@ -1,19 +1,5 @@
 const config = {
     branches: ["main", { name: "next", prerelease: true }],
-    plugins: [
-        [
-            "@semantic-release/commit-analyzer",
-            {
-                "preset": "angular",
-                "releaseRules": [
-                    { "type": "docs", "scope": "README", "release": "patch" },
-                    { "type": "refactor", "release": "major" },
-                    { "type": "revert", "release": "minor" },
-                ]
-            },
-        ],  
-        "@semantic-release/release-notes-generator"
-    ],
 };
 
 module.exports = config;
