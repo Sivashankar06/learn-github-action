02faf1bdebaacb0922775e3b256cf698dff033c8
Sivashankar <siva.a.shankar@gmail.com>
2024-10-01 13:08:04 +0530

    fix: print pr number

--- release.config.cjs
+++ release.config.cjs
@@ -1,30 +1,5 @@
 const config = {
     branches: ["main", { name: "next", prerelease: true }],
-    plugins: [
-        [
-          "@semantic-release/commit-analyzer",
-          {
-            preset: "angular",
-            releaseRules: [{ type: "revert", release: "patch" }],
-          },
-        ],
-        "@semantic-release/release-notes-generator",
-        [
-          "@semantic-release/github",
-          {
-            failTitle: false,
-            labels: false,
-            releasedLabels: false,
-            successComment:
-              ":tada: A prerelease with version ${nextRelease.version} has been published.\n".concat(
-                process.env.npm_package_name
-                  ? "To install run yarn add ${process.env.npm_package_name}@${nextRelease.version}"
-                  : "",
-              ),
-          },
-        ],
-        "semantic-release-yarn",
-      ],
 };
 
 module.exports = config;
